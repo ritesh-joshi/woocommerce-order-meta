e6bb18308d39f1652c65bfe3c67a4ac8eb799106
ritesh-joshi <30826589+ritesh-joshi@users.noreply.github.com>
2017-08-08 15:32:31 +0530

    Update order-meta.php

--- order-meta.php
+++ order-meta.php
@@ -1,23 +1,13 @@
 <?php 
-
-	class woocommerceOrderCustom{
-	
+class woocommerceOrderCustom{
 	function __construct(){
-  	  add_action( 'woocommerce_add_order_item_meta', array($this,'my_field_order_meta_handler'), 1, 3 );
-  }
-  function my_field_order_meta_handler( $item_id, $values, $cart_item_key ) {
-
-    if( isset( $values['design'] ) ) {
-        wc_add_order_item_meta( $item_id, "design", $values['design']);
+  	  		add_action( 'woocommerce_add_order_item_meta', array($this,'my_field_order_meta_handler'), 1, 3 );
+  	}
+  	
+	function my_field_order_meta_handler( $item_id, $values, $cart_item_key ) {
+		if( isset( $values['design'] ) ) {
+        		wc_add_order_item_meta( $item_id, "design", $values['design']);
 		}
-		
-	 if( isset( $values['general_information'] ) ) {
-        wc_add_order_item_meta( $item_id, "general_information", $values['general_information']);
-		}
-
-	if( isset( $values['attachment'] ) ) {
-        wc_add_order_item_meta( $item_id, "attachment", $values['attachment']);
-		}		
 	}
 } 
 $woocommerceOrderCustom = new woocommerceOrderCustom();
